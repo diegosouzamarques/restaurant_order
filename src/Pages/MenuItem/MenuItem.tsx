6b17dfaf569e929fad194882fa83b106056616ba
DESKTOP-66N0TI0\diego <diegomarquesfatec@gmail.com>
2023-04-18 16:05:32 -0300

    implements partial Menu item

--- src/Pages/MenuItem/MenuItem.tsx
+++ src/Pages/MenuItem/MenuItem.tsx
@@ -1,28 +1,46 @@
 import { CategoryMenu } from "../../Enum/CategoryMenu";
 import style from "./MenuItem.module.scss";
 import classNames from "classnames";
+import { useState } from "react";
 
 const MenuItem = () => {
   let menuOption = Object.values(CategoryMenu);
+  const [active, setActive] = useState(-1);
+  const onClick = (event: React.MouseEvent<HTMLLIElement>) => {
+    setActive(Number(event.currentTarget.id));
+  };
   return (
-    <ul className={style.menuOption}>
-      {menuOption.map((item, index) => {
-        let nameClass =
-          Object.keys(CategoryMenu)[Object.values(CategoryMenu).indexOf(item)];
-        return (
-          <li key={index}>
-            <i
-              className={classNames(
-                style.menuOption__icon,
-                style[`menuOption__icon_${nameClass}`]
-              )}
-            ></i>
-            <span>{}</span>
-            <span>{item}</span>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className={style.menuOption}>
+        {menuOption.map((item, index) => {
+          let nameClass =
+            Object.keys(CategoryMenu)[
+              Object.values(CategoryMenu).indexOf(item)
+            ];
+          return (
+            <li
+              id={String(index)} 
+              key={index}
+              className={classNames({ [style.menuOption__active]: index == active})}
+              onClick={onClick}
+            >
+              <i
+                className={classNames({
+                  [style.menuOption__icon]:true,
+                  [style[`menuOption__icon_${nameClass}`]]:true,
+                  [style[`menuOption__icon_${nameClass}_active`]]: index == active
+                })}
+              ></i> 
+              <span className={style.menuOption__title}>{item}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <h1  className={classNames({
+                  [style.menu]: active < 0,
+                  [style.menuSelected]: active > -1
+                })} ><span>{menuOption[active]}</span></h1>
+    </>
   );
 };
 
