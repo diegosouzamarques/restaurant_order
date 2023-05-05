564ecdf16ec31d68d37bafcb91905aece650d2db
DESKTOP-66N0TI0\diego <diegomarquesfatec@gmail.com>
2023-05-05 19:08:25 -0300

    ajustes

--- src/Pages/CloseOrder/CloseOrder.tsx
+++ src/Pages/CloseOrder/CloseOrder.tsx
@@ -32,35 +32,79 @@ const CloseOrder = () => {
     { title: "Coxinha Frango", qtd: 3, valor: 18.0 },
     { title: "Misto Quente", qtd: 3, valor: 65.0 },
     { title: "Sorvete Napolitano", qtd: 1, valor: 8.89 },
+
+    { title: "Chocolate Quente", qtd: 3, valor: 15.37 },
+    { title: "X-Tudão", qtd: 3, valor: 75.78 },
+    { title: "Batata Cheddar", qtd: 1, valor: 5.67 },
+    { title: "Cachorro Quente", qtd: 3, valor: 18.0 },
+    { title: "X-Salada", qtd: 3, valor: 65.0 },
+    { title: "Milk Shake", qtd: 1, valor: 8.32 },
+    { title: "Churros Doce Leite", qtd: 1, valor: 5.67 },
+    { title: "Coxinha Frango", qtd: 3, valor: 18.0 },
+    { title: "Misto Quente", qtd: 3, valor: 65.0 },
+    { title: "Sorvete Napolitano", qtd: 1, valor: 8.89 },
   ];
 
   useEffect(() => {
+    if (payMade.length <= 0 && discount.length <= 0) {
+      setTotal(
+        items.reduce((counter, item) => {
+          return counter + item.qtd * item.valor;
+        }, 0)
+      );
 
-    if((payMade.length <= 0) && (discount.length <= 0)){
-      setTotal(items.reduce((counter, item) => {
-        return counter + item.qtd * item.valor;
-      }, 0));
-  
-      setRest(items.reduce((counter, item) => {
-        return counter + item.qtd * item.valor;
-      }, 0));
-    }else{
+      setRest(
+        items.reduce((counter, item) => {
+          return counter + item.qtd * item.valor;
+        }, 0)
+      );
+    } else {
       cashierResume();
-    };
+    }
+  }, [payMade, discount, pay]);
 
-
-  },[payMade, discount, pay]);
-
-  const cashierResume = () =>{
-    setPay(payMade.reduce((counter, item) => {
-      return counter + item.value;
-    }, 0));
+  let array: PaymentMade[] = [
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+    { id: 1, name: "Dinheiro", value: 150.68 },
+  ];
+  const cashierResume = () => {
+    setPay(
+      payMade.reduce((counter, item) => {
+        return counter + item.value;
+      }, 0)
+    );
 
     let discountTotal = discount.reduce((counter, item) => {
       return counter + item.value;
     }, 0);
-    
-    setRest((total + discountTotal) - pay);
+
+    setRest(total + discountTotal - pay);
   };
 
   const opcoes: Option[] = [
@@ -71,10 +115,12 @@ const CloseOrder = () => {
   ];
 
   const addItem = (pay: PaymentMade) => {
-    if (pay) {
+    setPayMade(array);
+    setDiscount(array);
+    /*     if (pay) {
       if (pay.id === 5) setDiscount([...discount, pay]);
       else setPayMade([...payMade, pay]);
-    }
+    } */
     setShowPayment(false);
   };
 
@@ -85,93 +131,114 @@ const CloseOrder = () => {
   return (
     <>
       <Payment show={showPayment} addItem={addItem} cancelItem={cancelItem} />
+
       <div className={style.container}>
-        <Button
-          type="button"
-          nipple="order"
-          className={style.container__btn_pay}
-          onClick={(e) => {
-            e.preventDefault();
-            setShowPayment(true);
-          }}
-        >
-          Pagamento
-        </Button>
-        <div className={style.container__payment_details}>
-          <h3 className={classNames(style.container__payment_details__title)}>
-            Pagamentos:
-          </h3>
-          <ul>
-            {payMade.length <= 0 && (
-              <>
-                <hr />
-                <br />
-                <hr />
-              </>
-            )}
-
-            {payMade.map((item, index) => (
-              <li key={index}>
-                <span>{item.name}</span> <span>{item.value.toFixed(2)}</span>
-              </li>
-            ))}
-          </ul>
+        <div className={style.container__details_order}>
+          <Select
+            id="table"
+            title="Mesa"
+            options={opcoes}
+            selectedValue={opcoes[0]}
+          ></Select>
+          <Text title="Solicitante" text="Maria Marques"></Text>
+          <Text
+            title="Observação"
+            text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate."
+          ></Text>
         </div>
 
-        <div className={style.container__payment_details}>
-          <h3 className={style.container__payment_details__title}>
-            Desconto:{" "}
-          </h3>
-          <ul>
-            {discount.length <= 0 && (
-              <>
-                <hr />
-                <br />
-                <hr />
-              </>
-            )}
+        <div className={style.container__payment}>
+          <Button
+            type="button"
+            nipple="order"
+            className={style.container__payment__btn_pay}
+            onClick={(e) => {
+              e.preventDefault();
+              setShowPayment(true);
+            }}
+          >
+            Pagamento
+          </Button>
+          <div className={style.container__payment__cashier}>
 
-            {discount.map((item, index) => (
-              <li key={index}>
-                <span>{item.name}</span> <span>{item.value.toFixed(2)}</span>
-              </li>
-            ))}
-          </ul>
+            <div className={style.container__payment__details +" "+style.container__payment__cashier__item_1}>
+              <h3
+                className={classNames(style.container__payment__details__title)}
+              >
+                Pagamentos:
+              </h3>
+              {payMade.length <= 0 && (
+                <>
+                  <hr />
+                  <br />
+                  <hr />
+                </>
+              )}
+              <ul
+                className={classNames(
+                  style.container__payment__details__items_pay
+                )}
+              >
+                {payMade.map((item, index) => (
+                  <li key={index}>
+                    <span>{item.name +" "+ (index+1)}</span>
+                    <span>{item.value.toFixed(2)}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={style.container__payment__details +" "+style.container__payment__cashier__item_2}>
+              <h3 className={style.container__payment__details__title}>
+                Desconto:{" "}
+              </h3>
+              {discount.length <= 0 && (
+                <>
+                  <hr />
+                  <br />
+                  <hr />
+                </>
+              )}
+              <ul
+                className={classNames(
+                  style.container__payment__details__items_discont
+                )}
+              >
+                {discount.map((item, index) => (
+                  <li key={index}>
+                    <span>{item.name +" "+ (index+1)}</span>
+                    <span>{item.value.toFixed(2)}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={style.container__payment__details +" "+style.container__payment__cashier__item_3}>
+              <h3
+                className={classNames(style.container__payment__details__title)}
+              >
+                Resumo Caixa:
+              </h3>
+              <ul>
+                <li>
+                  <span>A Pagar</span> <span>{total.toFixed(2)}</span>
+                </li>
+                <li>
+                  <span>Pagamentos</span> <span>{pay.toFixed(2)}</span>
+                </li>
+                <li>
+                  <span>Restante</span> <span>{rest.toFixed(2)}</span>
+                </li>
+              </ul>
+            </div>
+
+          </div>
         </div>
 
-        <div className={style.container__payment_details}>
-          <h3 className={classNames(style.container__payment_details__title)}>
-            Resumo Caixa:
-          </h3>
-          <ul>
-            <li>
-              <span>A Pagar</span> <span>{total.toFixed(2)}</span>
-            </li>
-            <li>
-              <span>Pagamentos</span> <span>{pay.toFixed(2)}</span>
-            </li>
-            <li>
-              <span>Restante</span> <span>{rest.toFixed(2)}</span>
-            </li>
-          </ul>
+        <div className={style.container__items}>
+          <ListItem items={items} close={true}></ListItem>
         </div>
       </div>
-
-      <div className={style.details_order}>
-        <Select
-          id="table"
-          title="Mesa"
-          options={opcoes}
-          selectedValue={opcoes[0]}
-        ></Select>
-        <Text title="Solicitante" text="Maria Marques"></Text>
-        <Text
-          title="Observação"
-          text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate."
-        ></Text>
-      </div>
-
-      <ListItem items={items} close={true}></ListItem>
     </>
   );
 };
